8851ab24a1e95901464ace8cf8ac995020efeea7
Michael Fasani <michael.fasani@squirro.com>
2020-06-03 18:54:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# r3f boilerplate
+
+<img src="https://img.shields.io/david/fasani/r3f-boilerplate?color=green"> <img src="https://img.shields.io/david/dev/fasani/r3f-boilerplate?color=green"> <img src="https://img.shields.io/github/license/fasani/r3f-boilerplate?color=black">
+
+A lightweight boilerplate for r3f ([react-three-fiber](https://github.com/react-spring/react-three-fiber))
+
+## Features
+
+- [react-refresh](https://github.com/facebook/react/tree/master/packages/react-refresh) - Edit components without losing their state
+- [drei](https://github.com/react-spring/drei) - Helpers for react-three-fiber
+
+## Quick start
+
+1. Clone this repo using `git clone --depth=1 https://github.com/fasani/r3f-boilerplate.git <YOUR_PROJECT_NAME>`
+2. Move to the newly created directory: `cd <YOUR_PROJECT_NAME>`
+3. Run `npm install` to install dependencies
+3. Run `npm start` to see the example app at http://localhost:3000
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const htmlWebpack = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        port: 3000
+    },
+    mode: isDevelopment ? 'development' : 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: require.resolve('babel-loader'),
+                    options: {
+                        plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean)
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpack({
+            template: './src/index.html'
+        }),
+        isDevelopment && new ReactRefreshWebpackPlugin(),
+    ].filter(Boolean)
+};
