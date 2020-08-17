86029d35d73bc33c99e00c308644b3ae1728d323
Fasani <michaelfasani@gmail.com>
2020-08-17 16:40:38 +0200

    Adding Gitpod info to readme

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     devServer: {
         port: 3000,
         host: '0.0.0.0',
+        disableHostCheck: true,
         useLocalIp: true,
         open: true,
         hot: true,
